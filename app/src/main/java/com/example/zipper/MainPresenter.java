70956b1d60adb088d5f89ecff8ac726d0fdf8f67
Simon Edström <simon.edstrom@screeninteraction.com>
2017-01-24 23:13:46 +0100

    Bind two values to same view.

--- app/src/main/java/com/example/zipper/MainPresenter.java
+++ app/src/main/java/com/example/zipper/MainPresenter.java
@@ -6,6 +6,12 @@ public class MainPresenter {
 
     public void takeView(MainView view) {
         this.view = view;
-        this.view.bind(new MainViewModel("Text", "Hint", R.mipmap.ic_launcher , true, true, "Welcome!"));
+        this.view.bind(new MainViewModel(
+                "Text hello",
+                "Hint",
+                R.mipmap.ic_launcher,
+                true,
+                "Switch text", true,
+                "Welcome!"));
     }
 }
--- app/src/main/java/com/example/zipper/MainViewModel.java
+++ app/src/main/java/com/example/zipper/MainViewModel.java
@@ -11,7 +11,9 @@ import se.snylt.zipper.annotations.BindToView;
 import se.snylt.zipper.annotations.OnBind;
 import se.snylt.zipper.viewbinder.onbind.ToastLongOnBind;
 
+import static se.snylt.zipper.ViewProperties.CompoundButton.CHECKED;
 import static se.snylt.zipper.ViewProperties.EditText.HINT;
+import static se.snylt.zipper.ViewProperties.TextView.TEXT;
 
 public class MainViewModel {
 
@@ -27,18 +29,23 @@ public class MainViewModel {
     @BindToCompoundButton(id = R.id.main_activity_check_box)
     final public boolean checked;
 
-    @BindToView(view = Switch.class, id = R.id.main_activity_switch, set = "checked")
+    @BindToView(view = Switch.class, id = R.id.main_activity_switch, set = CHECKED)
     final public boolean switched;
+
+    @BindToView(view = Switch.class, id = R.id.main_activity_switch, set = TEXT)
+    final public String switchText;
 
     @BindTo(R.id.activity_main) // view
     @OnBind(ToastLongOnBind.class) // bind action
     final public String welcomeMessage;
 
-    public MainViewModel(String text, String hint, int image, boolean checked, boolean switched, String welcomeMessage) {
+    public MainViewModel(String text, String hint, int image, boolean checked, String switchText, boolean switched,
+            String welcomeMessage) {
         this.text = text;
         this.hint = hint;
         this.image = image;
         this.checked = checked;
+        this.switchText = switchText;
         this.switched = switched;
         this.welcomeMessage = welcomeMessage;
     }
