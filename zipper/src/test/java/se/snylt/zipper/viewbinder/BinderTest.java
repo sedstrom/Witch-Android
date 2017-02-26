656ea1075817eadd49fc3da6e10ef83783fa716c
Simon Edström <simon.edstrom@screeninteraction.com>
2017-02-26 14:45:19 +0100

    Test Binder.

---
+++ zipper/src/test/java/se/snylt/zipper/viewbinder/BinderTest.java
@@ -0,0 +1,50 @@
+package se.snylt.zipper.viewbinder;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+
+import se.snylt.zipper.viewbinder.viewfinder.ViewFinder;
+
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.verify;
+
+public class BinderTest {
+
+    @Mock
+    ViewBinder viewBinderOne;
+
+    @Mock
+    ViewBinder viewBinderTwo;
+
+    Object viewHolder = new Object();
+
+    Object target = new Object();
+
+    Object mod = new Object();
+
+    @Mock
+    ViewFinder viewFinder;
+
+    private Binder binder;
+
+    @Before
+    public void setup(){
+        MockitoAnnotations.initMocks(this);
+        binder = new Binder(Arrays.asList(viewBinderOne, viewBinderTwo));
+    }
+
+    @Test
+    public void bind_Should_CallBindOnEachViewBinder(){
+        // When
+        binder.bind(viewHolder, viewFinder, target, mod);
+
+        // Then
+        verify(viewBinderOne).bind(same(viewHolder), same(viewFinder), same(target), same(mod));
+        verify(viewBinderTwo).bind(same(viewHolder), same(viewFinder), same(target), same(mod));
+    }
+
+}
