5e1d98fda094b85914c13db212fae95e5b394a24
Simon Edström <simon.edstrom@screeninteraction.com>
2017-04-23 13:09:14 +0200

    Remove test code in App.

--- app/src/main/java/com/example/witch/App.java
+++ app/src/main/java/com/example/witch/App.java
@@ -2,13 +2,7 @@ package com.example.witch;
 
 import com.squareup.leakcanary.LeakCanary;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.animation.ObjectAnimator;
 import android.app.Application;
-import android.view.View;
-
-import se.snylt.witch.viewbinder.bindaction.OnBindListener;
 
 public class App extends Application{
 
@@ -21,18 +15,5 @@ public class App extends Application{
             return;
         }
         LeakCanary.install(this);
-
-        ObjectAnimator animator = ObjectAnimator.ofFloat(new View(this), View.ALPHA, 0f, 1f);
-        animator.setDuration(300);
-        animator.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                //
-                OnBindListener l = null;
-                l.onBindDone();
-            }
-        });
-        animator.start();
-
     }
 }
