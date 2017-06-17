5f8e7b8ac7374e46eb314a30b57e3f9ac960659c
Simon Edström <simon.edstrom@screeninteraction.com>
2017-06-17 16:04:14 +0200

    Update docs.

--- docs/_includes/snippets/11.java
+++ docs/_includes/snippets/11.java
@@ -1,3 +1,7 @@
 @BindToView(id = R.id.user_view, view = UserView.class, set = "user")
-@AlwaysBind
-Value<User> user; // Will be part of every bind pass
+@BindWhen(BindWhen.ALWAYS)
+User user; // Will be part of every bind pass
+
+@BindToTextView(R.id.message)
+@BindWhen(BindWhen.NOT_EQUALS)
+String message; // Will be bound when not equals to current value
