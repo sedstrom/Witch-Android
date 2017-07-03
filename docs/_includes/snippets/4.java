6b8d52e68f4c3204dac995d65a41721e16824dd1
Simon Edström <simon.edstrom@screeninteraction.com>
2017-07-03 19:35:41 +0200

    Update docs.

--- docs/_includes/snippets/4.java
+++ docs/_includes/snippets/4.java
@@ -1,8 +1,8 @@
 @BindTo(R.id.title)
 String title = "Abra cadabra!";
 
-@Bind
-Binder<TextView, String> bindTitle = Binder.create(new SyncOnBind<TextView, String>() {
+@Binds
+Binder<TextView, String> bindsTitle = Binder.create(new SyncOnBind<TextView, String>() {
   @Override
   public void onBind(TextView view, String title) {
     view.setText(title);
