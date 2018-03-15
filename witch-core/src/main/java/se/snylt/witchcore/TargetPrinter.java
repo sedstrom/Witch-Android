20024ca0c6f14e5e7202d923db92fb08a28512dc
Simon Edström <simon.edstrom@screeninteraction.com>
2018-03-15 07:30:29 +0100

    Clean up.

--- witch-core/src/main/java/se/snylt/witchcore/TargetPrinter.java
+++ witch-core/src/main/java/se/snylt/witchcore/TargetPrinter.java
@@ -6,19 +6,6 @@ public abstract class TargetPrinter<T> {
 
     private final HashMap<String, String> out = new HashMap<>();
 
-    String describeTarget(T target) {
-        out.clear();
-        describeTarget(target, out);
-        return target.getClass().getSimpleName() + " >\n" + out.toString();
-
-        /*
-        try {
-            return target.getClass().getSimpleName() + " >\n" + out.toString(); //new JSONObject(out).toString(2);
-        } catch (JSONException e) {
-            return target.getClass().getSimpleName() + " >\n" + new JSONObject(out).toString();
-        }*/
-    }
-
     public abstract void describeTarget(T target, HashMap<String, String> out);
 
 }
